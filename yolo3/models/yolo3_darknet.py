f83a0af92e12fedcc3c735d0e636ba050a6f74a9
ManoniLo <lorimano93@gmail.com>
2022-05-09 13:23:43 +0200

    Update yolo3_darknet.py

--- yolo3/models/yolo3_darknet.py
+++ yolo3/models/yolo3_darknet.py
@@ -346,7 +346,7 @@ def tiny_yolo3_custom_leaky_body(inputs, num_anchors, num_classes):
 
     # feature map 1 output (13x13 for 416 input)
     y1 = compose(
-        DarknetConv2D_BN_custom_Leaky(512, (3, 3)),
+        DarknetConv2D_BN_custom_Leaky(512, (3, 3), name = "anchor_1"),
         DarknetConv2D(num_anchors * (num_classes + 5), (1, 1), name="predict_conv_1"),
     )(x1)
 
@@ -356,7 +356,7 @@ def tiny_yolo3_custom_leaky_body(inputs, num_anchors, num_classes):
     # feature map 2 output (26x26 for 416 input)
     y2 = compose(
         Concatenate(),
-        DarknetConv2D_BN_custom_Leaky(256, (3, 3)),
+        DarknetConv2D_BN_custom_Leaky(256, (3, 3), name = "anchor_2"),
         DarknetConv2D(num_anchors * (num_classes + 5), (1, 1), name="predict_conv_2"),
     )([x2, f2])
 
