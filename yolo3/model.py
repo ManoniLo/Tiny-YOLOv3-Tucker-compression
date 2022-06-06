6680f499f9ce5e5612025d65c12a39f1e3c0443c
ManoniLo <lorimano93@gmail.com>
2022-06-06 11:49:08 +0200

    Add files via upload

--- yolo3/model.py
+++ yolo3/model.py
@@ -13,6 +13,7 @@ from tensorflow.keras.optimizers import Adam
 
 from yolo3.models.yolo3_darknet import (
     yolo3_body,
+    yolo3_truncated_body,
     custom_tiny_yolo3_body,
     custom_leaky_tiny_yolo3_body,
     custom_tiny_yolo3_body_no_leaky,
@@ -191,6 +192,7 @@ yolo3_model_map = {
     "yolo3_efficientnet_lite_spp": [yolo3lite_spp_efficientnet_body, 382, None],
     "yolo3_darknet": [yolo3_body, 185, "weights/darknet53.h5"],
     "yolo3_darknet_spp": [custom_yolo3_spp_body, 185, "weights/yolov3-spp.h5"],
+    "yolo3_darknet_truncated", [yolo3_truncated_body, 20, None],
     # Doesn't have pretrained weights, so no need to return backbone length
     "yolo3_darknet_lite": [yolo3lite_body, 0, None],
     "yolo3_vgg16": [yolo3_vgg16_body, 19, None],
