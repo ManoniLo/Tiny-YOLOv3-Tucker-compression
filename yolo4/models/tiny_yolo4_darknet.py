7b72d7a7c6c0bbbd4359c4bdeb9d91bb07873f9a
ManoniLo <lorimano93@gmail.com>
2022-12-30 15:30:59 +0100

    Update tiny_yolo4_darknet.py

--- yolo4/models/tiny_yolo4_darknet.py
+++ yolo4/models/tiny_yolo4_darknet.py
@@ -79,3 +79,35 @@ def darknet_body(x):
 
     feat2 = x
     return feat1, feat2
+  
+  
+  attention = [se_block, cbam_block, eca_block, ca_block]
+
+
+def yolo_body(input_shape, anchors_mask, num_classes, phi = 0):
+    inputs = Input(input_shape)
+    
+    feat1, feat2 = darknet_body(inputs)
+    if phi >= 1 and phi <= 4:
+        feat1 = attention[phi - 1](feat1, name='feat1')
+        feat2 = attention[phi - 1](feat2, name='feat2')
+
+    # 13,13,512 -> 13,13,256
+    P5 = DarknetConv2D_BN_Leaky(256, (1,1))(feat2)
+    # 13,13,256 -> 13,13,512 -> 13,13,255
+    P5_output = DarknetConv2D_BN_Leaky(512, (3,3))(P5)
+    P5_output = DarknetConv2D(len(anchors_mask[0]) * (num_classes+5), (1,1))(P5_output)
+    
+    # 13,13,256 -> 13,13,128 -> 26,26,128
+    P5_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P5)
+    if phi >= 1 and phi <= 4:
+        P5_upsample = attention[phi - 1](P5_upsample, name='P5_upsample')
+
+    # 26,26,256 + 26,26,128 -> 26,26,384
+    P4 = Concatenate()([P5_upsample, feat1])
+    
+    # 26,26,384 -> 26,26,256 -> 26,26,255
+    P4_output = DarknetConv2D_BN_Leaky(256, (3,3))(P4)
+    P4_output = DarknetConv2D(len(anchors_mask[1]) * (num_classes+5), (1,1))(P4_output)
+    
+    return Model(inputs, [P5_output, P4_output])
