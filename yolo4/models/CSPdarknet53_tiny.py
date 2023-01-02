77065a3f872e0472838009b9b750340aed967329
ManoniLo <lorimano93@gmail.com>
2023-01-02 10:33:00 +0100

    Update CSPdarknet53_tiny.py

--- yolo4/models/CSPdarknet53_tiny.py
+++ yolo4/models/CSPdarknet53_tiny.py
@@ -9,6 +9,13 @@ from tensorflow.keras.layers.normalization import BatchNormalization
 from tensorflow.keras.regularizers import l2
 
 
+def compose(*funcs):
+    if funcs:
+        return reduce(lambda f, g: lambda *a, **kw: g(f(*a, **kw)), funcs)
+    else:
+        raise ValueError('Composition of empty sequence not supported.')
+
+
 
 #from utils.utils import compose
 
