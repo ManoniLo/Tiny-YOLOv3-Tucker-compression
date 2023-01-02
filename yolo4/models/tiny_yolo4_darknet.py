719719ec1aa575e9cc69ae75f441f1e71ba83042
ManoniLo <lorimano93@gmail.com>
2023-01-02 11:04:33 +0100

    Update tiny_yolo4_darknet.py

--- yolo4/models/tiny_yolo4_darknet.py
+++ yolo4/models/tiny_yolo4_darknet.py
@@ -15,7 +15,7 @@ from yolo4.models.CSPdarknet53_tiny import (DarknetConv2D, DarknetConv2D_BN_Leak
                                             compose)
 
 
-def tiny_yolo4_body(inputs, anchors_mask, num_classes):
+def tiny_yolo4_body(inputs, num_anchors, num_classes):
     #inputs = Input(input_shape)
     
     feat1, feat2 = tiny_yolo_darknet_body(inputs)
@@ -27,19 +27,19 @@ def tiny_yolo4_body(inputs, anchors_mask, num_classes):
     P5 = DarknetConv2D_BN_Leaky(256, (1,1))(feat2)
     # 13,13,256 -> 13,13,512 -> 13,13,255
     P5_output = DarknetConv2D_BN_Leaky(512, (3,3))(P5)
-    P5_output = DarknetConv2D(len(anchors_mask[0]) * (num_classes+5), (1,1))(P5_output)
+    P5_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P5_output)
     
     # 13,13,256 -> 13,13,128 -> 26,26,128
     P5_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P5)
-    if phi >= 1 and phi <= 4:
-        P5_upsample = attention[phi - 1](P5_upsample, name='P5_upsample')
+    #if phi >= 1 and phi <= 4:
+    #    P5_upsample = attention[phi - 1](P5_upsample, name='P5_upsample')
 
     # 26,26,256 + 26,26,128 -> 26,26,384
     P4 = Concatenate()([P5_upsample, feat1])
     
     # 26,26,384 -> 26,26,256 -> 26,26,255
     P4_output = DarknetConv2D_BN_Leaky(256, (3,3))(P4)
-    P4_output = DarknetConv2D(len(anchors_mask[1]) * (num_classes+5), (1,1))(P4_output)
+    P4_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P4_output)
     
     return Model(inputs, [P5_output, P4_output])
 
