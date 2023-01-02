d6b65fe179555502e7ee19b6bd7ff8be6c308630
ManoniLo <lorimano93@gmail.com>
2023-01-02 12:00:44 +0100

    Update tiny_yolo4_darknet.py

--- yolo4/models/tiny_yolo4_darknet.py
+++ yolo4/models/tiny_yolo4_darknet.py
@@ -15,35 +15,41 @@ from yolo4.models.CSPdarknet53_tiny import (DarknetConv2D, DarknetConv2D_BN_Leak
                                             compose)
 
 
-def tiny_yolo4_body(inputs, num_anchors, num_classes, weights_path = None):
-    #inputs = Input(input_shape)
-    
+def base_model(inputs,num_anchors):
     feat1, feat2 = tiny_yolo_darknet_body(inputs)
-    #if phi >= 1 and phi <= 4:
-    #    feat1 = attention[phi - 1](feat1, name='feat1')
-    #    feat2 = attention[phi - 1](feat2, name='feat2')
 
     # 13,13,512 -> 13,13,256
     P5 = DarknetConv2D_BN_Leaky(256, (1,1))(feat2)
     # 13,13,256 -> 13,13,512 -> 13,13,255
     P5_output = DarknetConv2D_BN_Leaky(512, (3,3))(P5)
-    P5_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P5_output)
     
     # 13,13,256 -> 13,13,128 -> 26,26,128
     P5_upsample = compose(DarknetConv2D_BN_Leaky(128, (1,1)), UpSampling2D(2))(P5)
-    #if phi >= 1 and phi <= 4:
-    #    P5_upsample = attention[phi - 1](P5_upsample, name='P5_upsample')
 
     # 26,26,256 + 26,26,128 -> 26,26,384
     P4 = Concatenate()([P5_upsample, feat1])
     
     # 26,26,384 -> 26,26,256 -> 26,26,255
     P4_output = DarknetConv2D_BN_Leaky(256, (3,3))(P4)
-    P4_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P4_output)
-    
+
     model = Model(inputs, [P5_output, P4_output])
-    if weights_path is not None:
-      model.load_weights(weights_path,by_name = True, skip_mismatch = True)
-    
+
     return model
+
+def tiny_yolo4_body(inputs, num_anchors, num_classes, weights_path = None):
+
+  model_body = base_model(inputs,num_anchors)
+  P5_output, P4_output = model_body.outputs
+
+  P4_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P4_output)
+  P5_output = DarknetConv2D(num_anchors*  (num_classes+5), (1,1))(P5_output)
+
+
+  if weights_path is not None:
+      model_body.load_weights(weights_path)
+
+  
+  model = Model(inputs = inputs, outputs = [P5_output, P4_output])
+    
+  return model
 
