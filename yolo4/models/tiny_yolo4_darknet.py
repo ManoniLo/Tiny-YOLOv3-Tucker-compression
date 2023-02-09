089334e770609e9fd25650e005e17ec70142bda9
ManoniLo <lorimano93@gmail.com>
2023-02-09 10:01:30 +0100

    Add files via upload

--- yolo4/models/tiny_yolo4_darknet.py
+++ yolo4/models/tiny_yolo4_darknet.py
@@ -16,6 +16,8 @@ from yolo4.models.CSPdarknet53_tiny import (DarknetConv2D, DarknetConv2D_BN_Leak
 
 
 def base_model(inputs,num_anchors):
+
+    num_classes_coco = 80
     feat1, feat2 = tiny_yolo_darknet_body(inputs)
 
     # 13,13,512 -> 13,13,256
@@ -32,6 +34,10 @@ def base_model(inputs,num_anchors):
     # 26,26,384 -> 26,26,256 -> 26,26,255
     P4_output = DarknetConv2D_BN_Leaky(256, (3,3))(P4)
 
+    P5_output = DarknetConv2D(num_anchors*(num_classes_coco+5),(1,1),name = 'P5_out')(P5_output)
+    P4_output = DarknetConv2D(num_anchors*(num_classes_coco+5),(1,1),name = 'P4_out')(P4_output)
+
+
     model = Model(inputs, [P5_output, P4_output])
 
     return model
@@ -39,17 +45,23 @@ def base_model(inputs,num_anchors):
 def tiny_yolo4_body(inputs, num_anchors, num_classes, weights_path = None):
 
   model_body = base_model(inputs,num_anchors)
-  P5_output, P4_output = model_body.outputs
+  if weights_path:
+      model_body.load_weights(weights_path,by_name = False)
 
+  #P5_output, P4_output = model_body.outputs
+  P5_output = model_body.get_layer(name = 'P5_out').input
+  P4_output = model_body.get_layer(name = 'P4_out').input
+      
   P4_output = DarknetConv2D(num_anchors * (num_classes+5), (1,1))(P4_output)
   P5_output = DarknetConv2D(num_anchors*  (num_classes+5), (1,1))(P5_output)
   
   model = Model(inputs = inputs, outputs = [P5_output, P4_output])
-  
-  if weights_path is not None:
-      model.load_weights(weights_path,by_name = True, skip_mismatch = True)
+
+  #if weights_path is not None:
+  #    model.load_weights(weights_path,by_name = True, skip_mismatch = True)
+
 
   
-  
+    
   return model
 
